3b6c9e9810a264aa895493d91aac09d551879eaa
안태현 <68155601+kirikaka@users.noreply.github.com>
2025-03-24 09:35:49 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        
+        int max=array[0];
+        for(int i=1;i<array.length;i++){
+            if (array[i]>max){
+                max=array[i];
+                answer[0]=array[i];
+                answer[1]=i;
+            }
+        }
+        
+        
+        
+        return answer;
+    }
+}
