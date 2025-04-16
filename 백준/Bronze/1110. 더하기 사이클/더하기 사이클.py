b5698792236a5a9e33f4f0b1793708d2ef7b6543
안태현 <68155601+kirikaka@users.noreply.github.com>
2025-04-16 13:23:55 +0900

    [Bronze I] Title: 더하기 사이클, Time: 76 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -0,0 +1,22 @@
+n=int(input())
+og=n
+count=0
+var1=0
+while(True):
+    a=n%10
+    b=n//10
+    
+    if(a+b>=10):
+        var1=a+b-10
+    else:
+        var1=a+b
+
+
+    n=(a*10)+(var1)
+    count=count+1
+    if(n==og):
+        break
+    
+    
+
+print(count)
