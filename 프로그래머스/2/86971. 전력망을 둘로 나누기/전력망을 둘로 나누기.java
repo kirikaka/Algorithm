b1bbef48854ebc66be5e2f25daaf1bdef3fde469
안태현 <68155601+kirikaka@users.noreply.github.com>
2025-05-30 10:33:31 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.65 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,55 +3,49 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
-        // 모든 입력된 전선을 순회하며 끊었다 붙여가며 갯수를 센다.
-        ArrayList<Integer>[] graph=new ArrayList[n+1];
+        
+        ArrayList[] graph=new ArrayList[n+1];
         for(int i=1;i<=n;i++){
             graph[i]=new ArrayList<>();
+            System.out.println(graph[i]);
         }
-        for(int []wire : wires){
-            // graph만들기
+        for(int []wire:wires){
             graph[wire[0]].add(wire[1]);
             graph[wire[1]].add(wire[0]);
-            
         }
-        // 전선 끊어서 길이 확인
-        int max=0;
-        for(int[] wire:wires){
+        
+        for(int[] wire : wires){
             int a=wire[0];
             int b=wire[1];
             graph[a].remove(Integer.valueOf(b));
             graph[b].remove(Integer.valueOf(a));
-
-
-            System.out.println(graph[a]);
-                
+            
+            int count=1;
             boolean[] visited=new boolean[n+1];
-            ArrayDeque<Integer> queue=new ArrayDeque<>();
+            ArrayDeque<Integer>queue=new ArrayDeque<>();
             queue.add(a);
-            int cnt1=1;
+            
             visited[a]=true;
             
             while(!queue.isEmpty()){
-                int curNode=queue.poll();
-                for(int nextNode:graph[curNode]){
-                    if(!visited[nextNode]){
-                        cnt1++;
-                        queue.add(nextNode);
-                        visited[nextNode]=true;
+                int cur=queue.poll();
+                ArrayList<Integer> nexts = graph[cur];
+                for(int next : nexts){
+                    if(!visited[next]){
+                        visited[next]=true;
+                        count++;
+                        queue.add(next);
                     }
                 }
-                
             }
-            int cnt2=n-cnt1;
-            answer=Math.min(answer,Math.abs(cnt1-cnt2));
+            int count2=n-count;
+            answer=Math.min(answer,Math.abs(count2-count));
             
-            graph[wire[0]].add(wire[1]);
-            graph[wire[1]].add(wire[0]);
-            
+            graph[a].add(b);
+            graph[b].add(a);
             
             
         }
-        
         
         
         return answer;
