11a9e516491fc51a7e3c5e23f5c31ff2e343d51c
안태현 <68155601+kirikaka@users.noreply.github.com>
2025-05-30 09:25:53 +0900

    [level 3] Title: 단어 변환, Time: 0.70 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,59 +1,50 @@
 import java.util.*;
 
-
 class Solution {
     class Word{
-        int count;
         String word;
+        int count;
         Word(String word,int count){
-            this.count=count;
             this.word=word;
+            this.count=count;
         }
     }
-    public boolean onlyOneMatch(String word1,String word2){
-        int count =0;
+    boolean onlyOneMatch(String word1,String word2){
+        int count=0;
         for(int i=0;i<word1.length();i++){
-            if(word1.charAt(i)==word2.charAt(i)){
+            if(word1.charAt(i) != word2.charAt(i)){
                 count++;
             }
         }
-        if(word1.length()-count==1){
-            return true;
-        }        
+        if(count==1){
+          return true;
+        } 
         return false;
+        
     }
+    
     
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        // 단어의 방문 여부를 따지고. 두 단어마다 차이나는 알파벳 개수가 1개일때 이동하도록. 
-        // BFS로 방문하면 항상 최소이다. 
-        HashSet<String> visitWord= new HashSet<>();
-        ArrayDeque<Word> queue=new ArrayDeque<>(); 
-        visitWord.add(begin);
-        queue.offer(new Word(begin,0));
+        HashSet<String>visit=new HashSet<>();
         
+        visit.add(begin);
+        ArrayDeque<Word>queue=new ArrayDeque<>();
+        queue.add(new Word(begin, 0));
         while(!queue.isEmpty()){
-            Word current=queue.poll();
-            
-            String curWord=current.word;
-            int curCount=current.count;
-            
-            
-            if(curWord.equals(target)){
-                return curCount;
+            Word curWord=queue.poll();
+            // System.out.println(curWord.word+" "+curWord.count);
+            if(curWord.word.equals(target)){
+                return curWord.count;
             }
             
             for(String nextWord:words){
-                if(!visitWord.contains(nextWord) && onlyOneMatch(curWord,nextWord)){
-                    queue.offer(new Word(nextWord,curCount+1));
-                    visitWord.add(nextWord);
+                if(onlyOneMatch(curWord.word,nextWord) && !visit.contains(nextWord)){
+                    queue.add(new Word(nextWord,curWord.count+1));
+                    visit.add(nextWord);
                 }
             }
-            
-            
-            
         }
-        
         
         
         
