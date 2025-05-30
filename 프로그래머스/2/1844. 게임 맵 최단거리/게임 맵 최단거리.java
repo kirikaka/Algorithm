ec020bb24f7bd08474655db269179ff60f406678
안태현 <68155601+kirikaka@users.noreply.github.com>
2025-05-30 09:17:02 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.56 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -4,44 +4,42 @@ class Solution {
     public int solution(int[][] maps) {
         int answer = -1;
         
-        int n=maps.length;
-        int m=maps[0].length;
+        int rowLength=maps.length;
+        int colLength=maps[0].length;
         
-        boolean[][] visited=new boolean[n+1][m+1];
+        boolean[][] visited=new boolean[rowLength+1][colLength+1];
+        ArrayDeque<int[]> queue=new ArrayDeque<>();
         
-        ArrayDeque<Integer[]> queue= new ArrayDeque<>();
-        queue.add(new Integer[]{0,0,1});
+        queue.add(new int[]{0,0,1});
         visited[0][0]=true;
-        
-        int[] dr={0,0,-1,1};
-        int[] dc={1,-1,0,0};
+        int[] dr={-1,1,0,0};
+        int[] dc={0,0,1,-1};
         
         while(!queue.isEmpty()){
-            Integer []cur=queue.poll();
+            int []cur=queue.poll();
+            
             int curRow=cur[0];
             int curCol=cur[1];
             int curDist=cur[2];
-            if(curRow==n-1&&curCol==m-1){
-                answer=curDist;
-                break;
+            
+            if(curRow==rowLength-1 && curCol==colLength-1){
+                return curDist;
             }
             
             
             for(int i=0;i<4;i++){
-                int nextRow=dr[i]+curRow;
-                int nextCol=dc[i]+curCol;
-                int nextDist=curDist+1;
-                if(nextRow>=0 && nextRow<n && nextCol>=0 && nextCol<m){
+                int nextRow=curRow+dr[i];
+                int nextCol=curCol+dc[i];
+                
+                
+                if(nextRow>=0 && nextRow<rowLength && nextCol>=0 && nextCol<colLength){
                     if(!visited[nextRow][nextCol] && maps[nextRow][nextCol]==1){
-                        queue.offer(new Integer[]{nextRow,nextCol,nextDist});
+                        queue.add(new int[]{nextRow,nextCol,curDist+1});
                         visited[nextRow][nextCol]=true;
                     }
                 }
                 
-                
             }
-            
-            
             
         }
         
