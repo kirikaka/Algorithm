24fb5d81ae2da1e1c2d346b1a9ac9d7d5ebad83b
안태현 <68155601+kirikaka@users.noreply.github.com>
2025-07-03 23:25:06 +0900

    [level 2] Title: 소수 찾기, Time: 121.33 ms, Memory: 99 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Solution {
+    Set<Integer>primeSet;
+    
+    public int solution(String numbers) {
+        primeSet=new HashSet<>();
+        boolean[] visited=new boolean[numbers.length()];
+        backtrack(numbers,visited,"");
+        
+        
+        return primeSet.size();
+    }
+    void backtrack(String numbers,boolean[] visited,String cur){
+        if(cur.length()!=0){
+            int num=Integer.parseInt(cur);
+            if(isPrime(num)) primeSet.add(num);
+        }
+        
+        for(int i=0;i<numbers.length();i++){
+            if(!visited[i]){
+                visited[i]=true;
+                backtrack(numbers,visited,cur+numbers.charAt(i));
+                visited[i]=false;
+            }
+        }       
+        
+    }
+    boolean isPrime(int num){
+        if(num<2) return false;
+        for(int i=2;i<num;i++){
+            if(num%i==0) return false;
+        }
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
