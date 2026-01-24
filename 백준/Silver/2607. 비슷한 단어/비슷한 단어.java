a975774b0d9db57db2e89d19763a45664e6b6f5e
안태현 <68155601+kirikaka@users.noreply.github.com>
2026-01-24 11:17:53 +0900

    [Silver II] Title: 비슷한 단어, Time: 96 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
@@ -34,19 +34,23 @@ public class Main {
 
 
     static boolean check(String s){
+        // 단어 길이차이가 1보다 크면 당연히 실패
         if(Math.abs(s.length()-baseWord.length())>1) return false;
 
+        //현재 단어 구성 확인
         int[]cur=new int[26];
         for(int i=0;i<s.length();i++){
             cur[s.charAt(i)-'A']++;
         }
         int plus=0,minus=0;
+        // 첫단어랑 비교단어를 차를 계산함 
+        // 첫단어에 없으면 minus에, 비교단어에 없으면 plus에 차이 만큼 더함
         for(int i=0;i<26;i++){
             int diff=cur[i]-words[i];
             if(diff>0) plus+=diff;
             else minus+=-diff;
         }
-
+        //plus, minus가 아래와 같지 않으면 (=구성이 너무 다르면) false
         return (plus == 0 && minus == 0) ||
                 (plus == 1 && minus == 0) ||
                 (plus == 0 && minus == 1) ||
